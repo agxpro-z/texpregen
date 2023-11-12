90f0b904ae6de47bae7bf49334252c918ee65ad0
AnkitGourav <Ankitgourav035@gmail.com>
2023-11-12 11:48:02 +0530

    [UI][NavigationMenu]: Highlight active item.

--- app/ui/components/Menu.js
+++ app/ui/components/Menu.js
@@ -1,13 +1,18 @@
+"use client"
+
+import { usePathname } from "next/navigation"
 import Link from "next/link"
+import clsx from "clsx"
 import styles from "./menu.module.css"
 
 export default function Menu({ list }) {
+  const pathName = usePathname();
   return (
     <ul>
       {list.map((item) => {
         return (
           <li key="" className={styles.menuitem}>
-            <Link href={item.link} className={styles.link}>{item.name}</Link>
+            <Link href={item.link} className={clsx(styles.link, {[styles.active]: pathName == item.link})}>{item.name}</Link>
           </li>
         )
       })}
