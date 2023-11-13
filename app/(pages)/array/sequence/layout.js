dd1a3fa3bc03e2c80a9e788652ea8cb093226105
AnkitGourav <Ankitgourav035@gmail.com>
2023-11-13 13:47:52 +0530

    [Array/Sequence] Add metadata (title and description).

---
+++ app/(pages)/array/sequence/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: 'Array - Sequence',
+  description: 'Generate Array Sequence',
+}
+
+export default function ArraySequenceLayout({ children }) {
+  return (
+    <>{children}</>
+  )
+}
